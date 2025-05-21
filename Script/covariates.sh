24b9ae7cc97a1d30a7be32ccadbfefe1d6beb0b3
Fion Tan <ft824@login-b.cx3.hpc.ic.ac.uk>
2025-05-21 06:11:16 +0100

    update covariate code

--- Script/covariates.sh
+++ Script/covariates.sh
@@ -7,4 +7,4 @@ cd /rds/general/user/ft824/home/Term3-project/Script
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate r442
 
-Rscript covariates.R
+Rscript covariates2.R
--- Script/covariates2.R
+++ Script/covariates2.R
@@ -7,8 +7,8 @@ setwd("/rds/general/user/ft824/home/Term3-project/Data")
 stw_sf <- st_read("STW/stw_catchment_FINAL.shp")
 lsoa_sf <- st_read("LSOA2021_boundaries/LSOA2021_boundaries.shp")
 
-
-carehome_df <- read.csv("Covariates/lsoa/lsoa_carehome_density.csv")  
+#load school location density csv
+school_df <- read.csv("Covariates/lsoa/lsoa_school_density.csv")  
 
 # Add area to LSOAs
 lsoa_sf <- lsoa_sf %>%
@@ -27,8 +27,8 @@ lsoa_stw <- lsoa_stw_intersection %>%
     by = "LSOA21CD"
   ) %>%
   left_join(
-    carehome_df %>% 
-      select(LSOA21CD, carehome_den, population),
+    school_df %>% 
+      select(LSOA21CD, school_den, population),
     by = "LSOA21CD"
   )
 
@@ -39,13 +39,13 @@ lsoa_stw <- lsoa_stw %>%
     pop_in_stw = population * pop_fraction
   )
 
-stw_carehome <- lsoa_stw %>%
+stw_school <- lsoa_stw %>%
   group_by(site_code) %>%
   summarise(
-    weighted_carehome_density = sum(carehome_den * pop_in_stw, na.rm = TRUE) / 
+    weighted_school_density = sum(school_den * pop_in_stw, na.rm = TRUE) / 
                                 sum(pop_in_stw, na.rm = TRUE),
     .groups = "drop"
   )
 
 # Save result
-write_csv(stw_carehome, "/rds/general/user/ft824/home/Term3-project/Data/stw_carehome_density_summary.csv")
+write_csv(stw_school, "/rds/general/user/ft824/home/Term3-project/Data/stw_school_density.csv")
