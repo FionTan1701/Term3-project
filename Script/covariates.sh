e418d0bf47d1b725bc9634e68657a8d9975d49ef
Fion Tan <ft824@login-b.cx3.hpc.ic.ac.uk>
2025-06-12 13:25:53 +0100

    update inla scripts

--- Script/covariates.sh
+++ Script/covariates.sh
@@ -1,10 +1,10 @@
 #PBS -l walltime=72:00:00
 #PBS -l select=1:ncpus=4:mem=600gb
-#PBS -N inla_model1
+#PBS -N 4_cov_processing
 
-cd /rds/general/user/ft824/home/Term3-project/Script
+cd /rds/general/user/ft824/home/Term3-project/Script/data_processing
 
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate r442
 
-Rscript model1.R
+Rscript covariates_era5_v2.R
