2a033ba7705869bfa5e46140d7f6d7c8e85062ce
FionTan <fiontan@ymail.com>
2025-06-14 21:33:53 +0100

    update mesh plot

--- Script/inla_model.R
+++ Script/inla_model.R
@@ -6,16 +6,61 @@ library(viridis)
 library(sp)
 library(fmesher)
 library(GGally)
+library(sf)
+library(ggplot2)
 
 #set directory
 setwd("~/Term3-project")
 
 #read data
-nov_df <- read.csv("/Data/final_df1.csv")
+nov_df <- read.csv("Data/final_df3.csv")
+
+#create s_index
+nov_df <- nov_df %>%
+  arrange(site_code) %>%
+  mutate(site_code= as.factor(site_code)) %>%
+  mutate(s_index=as.numeric(site_code)) %>%
+  mutate(site_code= as.character(site_code))
 
 #mesh construction
+pdf("mesh.pdf", width = 14, height = 10)  # Open PDF device
 
 coords.cv = unique(nov_df[c("Easting", "Northing")])
-boundary.cv = inla.convex.hull(as.matrix(coords.cv[,1:2]))
+boundary.cv = inla.nonconvex.hull(as.matrix(coords.cv[,1:2]))
 mesh.cv = inla.mesh.2d(boundary = boundary.cv, max.edge = c(0.8, 1.3), cutoff = 0.1)
 plot(mesh.cv)
+
+ggplot() +
+  gg(mesh.cv) +
+  geom_point(data = coords.cv, aes(Longitude, Latitude)) 
+
+dev.off()
+##############
+
+
+pdf("mesh2.pdf", width = 14, height = 10)
+
+coords<- unique(st_coordinates(nov_df))
+
+#domain <- inla.nonconvex.hull(coords, concave = -0.05, convex = -0.02, resolution=c(200,200))
+domain <- inla.nonconvex.hull(coords)
+
+max.edge = 15
+bound.outer = diff(range(st_coordinates(nov_df)[,1]))/3
+
+mesh<- fm_mesh_2d_inla(boundary = domain,
+                       loc=coords,
+                       max.edge = c(1,2)*max.edge,
+                       offset= c(max.edge, bound.outer),
+                       cutoff= max.edge/5,
+                       crs= st_crs(nov_df))
+plot(mesh)
+
+ggplot() +
+  gg(mesh) +
+  geom_point(data = coords, aes(Longitude, Latitude)) 
+
+dev.off()
+
+##############
+
